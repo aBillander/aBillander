f2dfe1236a5484d397e11ee032bae1ffb0d36ea0
buzkall <buzkall@gmail.com>
2019-09-23 11:54:54 +0200

    show tax configuration in customers list

--- resources/views/customers/_panel_customer_users.blade.php
+++ resources/views/customers/_panel_customer_users.blade.php
@@ -1,34 +1,27 @@
+<div id="panel_customerusers">
 
-<div id="panel_customerusers">     
+    <div class="panel panel-info">
 
-<div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title">{{ l('Customer Center Access') }}</h3>
+        </div>
+        <div class="panel-body">
 
-   <div class="panel-heading">
-      <h3 class="panel-title">{{ l('Customer Center Access') }}</h3>
-   </div>
-               <div class="panel-body">
+            <div id="msg-customeruser-success" class="alert alert-success alert-block" style="display:none;">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <span id="msg-customeruser-success-counter" class="badge"></span>
+                <strong>{!!  l('This record has been successfully created &#58&#58 (:id) ', ['id' => ''], 'layouts') !!}</strong>
+            </div>
 
+            <!-- Customer Users --><!-- http://www.w3schools.com/bootstrap/bootstrap_ref_js_collapse.asp -->
 
-<div id="msg-customeruser-success" class="alert alert-success alert-block" style="display:none;">
-  <button type="button" class="close" data-dismiss="alert">&times;</button>
-  <span id="msg-customeruser-success-counter" class="badge"></span>
-  <strong>{!!  l('This record has been successfully created &#58&#58 (:id) ', ['id' => ''], 'layouts') !!}</strong>
-</div>
+            <div class="content_customerusers"></div>
 
+            <!-- Customer Users ENDS -->
 
+        </div><!-- div class="panel-body" -->
 
-
-<!-- Customer Users --><!-- http://www.w3schools.com/bootstrap/bootstrap_ref_js_collapse.asp -->
-
-<div class="content_customerusers"></div>
-
-<!-- Customer Users ENDS -->
-
-               </div><!-- div class="panel-body" -->
-
-
-
-</div><!-- div class="panel panel-info" -->
+    </div><!-- div class="panel panel-info" -->
 
 </div><!-- div id="panel_customeruser" -->
 
@@ -38,17 +31,17 @@
 @include('customers/_modal_customeruser_cart')
 
 
-@section('scripts')     @parent
+@section('scripts')
+    @parent
 <script type="text/javascript">
-   
-   $(document).ready(function() {
 
+    $(document).ready(function () {
 
-        $(document).on('click', '.create-customeruser', function(evnt) {
+        $(document).on('click', '.create-customeruser', function (evnt) {
 
             var id = $(this).attr('data-id');
 
-          	// Initialize
+            // Initialize
             $('#customeruser_action').val('create');
             $('#customeruser_id').val(id);
 
@@ -66,10 +59,10 @@
             $("input[name='display_prices_tax_inc'][value='-1']").prop('checked', true);
             $("input[name='notify_customer'][value='1']").prop('checked', true);
 
-          	// Open popup
+            // Open popup
             $('#customeruserModalTitle').html('{{ l('Create User') }} :: {{ $customer->name_commercial }}');
-            $("#msg-customeruser-error").find("ul").empty(  );
-            $("#msg-customeruser-error").css('display','none');
+            $("#msg-customeruser-error").find("ul").empty();
+            $("#msg-customeruser-error").css('display', 'none');
 
             $('#customeruserModal').modal({show: true});
             $("#customeruser_firstname").focus();
@@ -79,51 +72,45 @@
         });
 
 
-        $(document).on('click', '.update-customeruser', function(evnt) {
+        $(document).on('click', '.update-customeruser', function (evnt) {
 
             var id = $(this).attr('data-id');
 
-            var data = getCustomerUserData( id );
+            var data = getCustomerUserData(id);
 
             return false;
         });
-      
-
-   });
-
-   		$(window).on('hashchange',function(){
-			page = window.location.hash.replace('#','');
-			if (page == 'customerusers') getCustomerUsers();
-		});
 
 
-    function getCustomerUsers(){
+    });
 
-      $.ajax({
-        url: '{{ route( 'customer.getusers', [$customer->id] ) }}',
-        data: {
-          
-        }
-      }).done(function(data){
-        $('.content_customerusers').html(data);
-      });
+    $(window).on('hashchange', function () {
+        page = window.location.hash.replace('#', '');
+        if (page == 'customerusers') getCustomerUsers();
+    });
+
+
+    function getCustomerUsers() {
+
+        $.ajax({
+            url: '{{ route( 'customer.getusers', [$customer->id] ) }}',
+            data: {}
+        }).done(function (data) {
+            $('.content_customerusers').html(data);
+        });
     }
 
 
+    function getCustomerUserData(id) {
 
+        var url = "{{ route('customeruser.getuser', ':id') }}";
+        url = url.replace(':id', id);
 
-    function getCustomerUserData( id ){
-
-      var url = "{{ route('customeruser.getuser', ':id') }}";
-      url = url.replace(':id', id);
-      
-      $.ajax({
-        url: url,
-        data: {
-          
-        }
-      }).done(function(data){
-            console.log(data);
+        $.ajax({
+            url: url,
+            data: {}
+        }).done(function (data) {
+            //console.log(data);
             user = data.user;
 
             // Initialize
@@ -137,27 +124,25 @@
             $('#location_id').val(user.address_id);
             $('#min_order_value').val(user.min_order_value);
 
-            $("input[name='customeruser_active'][value='"+user.active+"']").prop('checked', true);
-            $("input[name='enable_quotations'][value='"+user.enable_quotations+"']").prop('checked', true);
-            $("input[name='enable_min_order'][value='"+user.enable_min_order+"']").prop('checked', true);
-            $("input[name='allow_abcc_access'][value='"+user.allow_abcc_access+"']").prop('checked', true);
-            $("input[name='display_prices_tax_inc'][value='"+user.display_prices_tax_inc+"']").prop('checked', true);
+            $("input[name='customeruser_active'][value='" + user.active + "']").prop('checked', true);
+            $("input[name='enable_quotations'][value='" + user.enable_quotations + "']").prop('checked', true);
+            $("input[name='enable_min_order'][value='" + user.enable_min_order + "']").prop('checked', true);
+            $("input[name='allow_abcc_access'][value='" + user.allow_abcc_access + "']").prop('checked', true);
+            $("input[name='display_prices_tax_inc'][value='" + user.display_prices_tax_inc + "']").prop('checked', true);
 //            $("input[name='notify_customer'][value='"+user.+"']").prop('checked', true);
-            $("#div-notify_customer").css('display','none');
+            $("#div-notify_customer").css('display', 'none');
 
             // Open popup
             $('#customeruserModalTitle').html('{{ l('Update User') }} :: {{ $customer->name_commercial }}');
-            $("#msg-customeruser-error").find("ul").empty(  );
-            $("#msg-customeruser-error").css('display','none');
+            $("#msg-customeruser-error").find("ul").empty();
+            $("#msg-customeruser-error").css('display', 'none');
 
             $('#customeruserModal').modal({show: true});
             $("#customeruser_firstname").focus();
-      });
+        });
 
-      // return false;
+        // return false;
     }
-
-
 
 </script>
 @endsection

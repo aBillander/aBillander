69a3c1266c8e6fbcf9ad1525d1a76fc7f953d006
Jose San <onagro@gmx.com>
2019-09-27 12:27:06 +0200

    CommissionSettlement Model & Controller

---
+++ app/Http/Controllers/CommissionSettlementsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CommissionSettlement;
+use Illuminate\Http\Request;
+
+class CommissionSettlementsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CommissionSettlement  $commissionSettlement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CommissionSettlement $commissionSettlement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CommissionSettlement  $commissionSettlement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CommissionSettlement $commissionSettlement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CommissionSettlement  $commissionSettlement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CommissionSettlement $commissionSettlement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CommissionSettlement  $commissionSettlement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CommissionSettlement $commissionSettlement)
+    {
+        //
+    }
+}

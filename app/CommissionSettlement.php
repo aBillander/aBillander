ddbeca88ae8e493175273e6d2af45338b32c45ee
Lara Billander <wasp@abillander.com>
2019-10-18 09:19:42 +0200

    [PATCH] CommissionSettlement Model & Controller

---
+++ app/CommissionSettlement.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CommissionSettlement extends Model
+{
+    // https://help.sap.com/viewer/085edb30fb3d413da552832f3d5c01c0/1808.500/en-US/b5ae5b578c9e4f3581685fb8979da28f.html
+}

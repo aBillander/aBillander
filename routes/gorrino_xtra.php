5e288c94a4aae7476630d9df73437d6ca021ca2a
OnagroSan <onagrosan@gmail.com>
2020-03-02 12:41:26 +0100

    Transitional

--- routes/gorrino_xtra.php
+++ routes/gorrino_xtra.php
@@ -28,103 +28,66 @@
 
 Route::get('migratethis_xtra', function()
 {
-  // 2020-02-09
-  Illuminate\Support\Facades\DB::statement("ALTER TABLE `customer_shipping_slips` ADD `shipment_service_type_tag` VARCHAR(32) NULL AFTER `shipment_status`;");
+  // 2020-03-02
+  Illuminate\Support\Facades\DB::statement("ALTER TABLE `cart_lines` ADD `pmu_label` varchar(128) null AFTER `pmu_conversion_rate`;");
+
+  $tables = ['customer_invoice', 'customer_shipping_slip', 'customer_quotation', 'customer_order'];
+
+  foreach ($tables as $table) {
+    # code...
+    Illuminate\Support\Facades\DB::statement("ALTER TABLE `".$table."_lines` ADD `pmu_label` varchar(128) null AFTER `pmu_conversion_rate`;");
+
+  }
+
+  Illuminate\Support\Facades\DB::statement("ALTER TABLE `customer_orders` ADD `onhold` TINYINT(4) NOT NULL DEFAULT '0' AFTER `status`;");
 
 
-  die('OK');
 
-  // 2020-01-26
-  Illuminate\Support\Facades\DB::statement("ALTER TABLE `production_orders` ADD `measure_unit_id` INT(10) UNSIGNED NULL AFTER `finished_quantity`;");
 
-  Illuminate\Support\Facades\DB::statement("ALTER TABLE `production_order_lines` ADD `real_quantity` DECIMAL(20,6) NULL AFTER `required_quantity`;");
-
-  // 2020-01-26
-  Illuminate\Support\Facades\DB::statement("DROP TABLE IF EXISTS `stock_movements`;");
-
-	Illuminate\Support\Facades\DB::statement("CREATE TABLE `stock_movements` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `stockmovementable_id` int(11) NOT NULL,
-  `stockmovementable_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `document_reference` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `quantity_before_movement` decimal(20,6) NOT NULL,
-  `quantity` decimal(20,6) NOT NULL,
-  `quantity_after_movement` decimal(20,6) NOT NULL,
-  `cost_price_before_movement` decimal(20,6) DEFAULT NULL,
-  `cost_price_after_movement` decimal(20,6) DEFAULT NULL,
-  `price` decimal(20,6) DEFAULT NULL,
-  `price_currency` decimal(20,6) DEFAULT NULL,
-  `currency_id` int(10) UNSIGNED NOT NULL,
-  `conversion_rate` decimal(20,6) NOT NULL,
-  `notes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `product_id` int(10) UNSIGNED NOT NULL,
-  `combination_id` int(10) UNSIGNED DEFAULT NULL,
-  `reference` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `warehouse_id` int(10) UNSIGNED NOT NULL,
-  `warehouse_counterpart_id` int(10) UNSIGNED DEFAULT NULL,
-  `movement_type_id` smallint(5) UNSIGNED NOT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `inventorycode` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL
+  Illuminate\Support\Facades\DB::statement("CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
 
-	Illuminate\Support\Facades\DB::statement("ALTER TABLE `stock_movements`
-  ADD PRIMARY KEY (`id`);");
+  Illuminate\Support\Facades\DB::statement("ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+");
 
-	Illuminate\Support\Facades\DB::statement("ALTER TABLE `stock_movements`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;");
-
-	Illuminate\Support\Facades\DB::statement("ALTER TABLE `stock_movements` ADD `measure_unit_id` INT(10) UNSIGNED NOT NULL AFTER `quantity`;");
-
-
-	// die('OK');
+  Illuminate\Support\Facades\DB::statement("ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  ");
+  
 
 
-	// 2020-01-25
-		Illuminate\Support\Facades\DB::statement("ALTER TABLE `production_orders` ADD `finish_date` timestamp null DEFAULT NULL AFTER `schedule_sort_order`;");
+  Illuminate\Support\Facades\DB::statement("CREATE TABLE `jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
+
+  Illuminate\Support\Facades\DB::statement("ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jobs_queue_index` (`queue`);
+");
+
+  Illuminate\Support\Facades\DB::statement("ALTER TABLE `jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  ");
 
 
-	die('OK');
+    Illuminate\Support\Facades\DB::statement("ALTER TABLE `measure_units` CHANGE `conversion_rate` `type_conversion_rate` DECIMAL(20,6) NOT NULL DEFAULT '1.000000';");
 
-	// 2020-01-23
-		
-		$o = \App\CustomerOrder::find(716);
+    
+    Illuminate\Support\Facades\DB::statement("ALTER TABLE `products` ADD `position` INT(10) NOT NULL DEFAULT '0' AFTER `name`;");
 
-		$o->document_prefix = null;
-
-		$o->document_id= 0;
-		$o->save();
-
-
-	die('OK');
-
-
-	// 2020-01-20
-		Illuminate\Support\Facades\DB::statement("ALTER TABLE `delivery_routes` ADD `carrier_id` INT(10) UNSIGNED NOT NULL AFTER `notes`;");
-
-
-	die('OK');
-
-	// 2020-01-17
-		Illuminate\Support\Facades\DB::statement("ALTER TABLE `customer_shipping_slips` ADD `production_sheet_id` INT(10) UNSIGNED NULL AFTER `customer_viewed_at`;");
-
-
-	die('OK');
-
-	// 2019-12-11
-		Illuminate\Support\Facades\DB::statement("ALTER TABLE `customers` ADD `invoice_sequence_id` INT(10) UNSIGNED NULL AFTER `bank_account_id`;");
-
-
-	die('OK');
-
-	// 2020-01-13
-		Illuminate\Support\Facades\DB::statement("ALTER TABLE `customers` ADD `shipping_slip_template_id` INT(10) UNSIGNED NULL AFTER `invoice_template_id`;");
-
-		Illuminate\Support\Facades\DB::statement("ALTER TABLE `customers` ADD `order_template_id` INT(10) UNSIGNED NULL AFTER `invoice_template_id`;");
 
 
 	die('OK');

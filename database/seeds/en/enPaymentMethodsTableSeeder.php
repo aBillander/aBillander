3dc5fbb0e40e3f6ea92de63638205175d19a43cf
Lara Billander <wasp@abillander.com>
2020-10-22 12:40:55 +0200

    hello-abi (IX). "en" Seeder

--- database/seeds/en/enPaymentMethodsTableSeeder.php
+++ database/seeds/en/enPaymentMethodsTableSeeder.php
@@ -5,13 +5,13 @@ use Illuminate\Database\Seeder;
 use App\PaymentMethod;
 use App\Configuration;
   
-class esPaymentMethodsTableSeeder extends Seeder {
+class enPaymentMethodsTableSeeder extends Seeder {
   
     public function run() {
         PaymentMethod::truncate();
   
         $pmeth = PaymentMethod::create( [
-            'name' => 'Contado / Efectivo', 
+            'name' => 'Cash', 
             'deadlines' => [['slot' => 0, 'percentage' => 100]],
             'payment_is_cash' => 1,
             'auto_direct_debit' => 0,
---
+++ database/seeds/en/enPaymentTypesTableSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\PaymentType;
+use App\Configuration;
+  
+class enPaymentTypesTableSeeder extends Seeder {
+  
+    public function run() {
+        PaymentType::truncate();
+  
+        $ptype = PaymentType::create( [
+            'alias' => 'Cash', 
+            'name' => 'Cash', 
+            'active' => 1, 
+            'accounting_code' => '',
+        ] );
+  
+        $ptype = PaymentType::create( [
+            'alias' => 'Remitance', 
+            'name' => 'Remitance', 
+            'active' => 1, 
+            'accounting_code' => '',
+        ] );
+  
+        $ptype = PaymentType::create( [
+            'alias' => 'Cheque', 
+            'name' => 'Cheque', 
+            'active' => 1, 
+            'accounting_code' => '',
+        ] );
+  
+        $ptype = PaymentType::create( [
+            'alias' => 'Bank Transfer', 
+            'name' => 'Transfer', 
+            'active' => 1, 
+            'accounting_code' => '',
+        ] );
+
+    }
+}
+
+/*
+
+INSERT INTO `payment_types` 
+(`id`, `alias`, `name`, `active`, `accounting_code`, `created_at`, `updated_at`, `deleted_at`) VALUES 
+(3, 'Efectivo', 'Efectivo', 1, '100', '2019-10-25 14:12:09', '2019-10-25 14:12:09', NULL), 
+(4, 'Remesa', 'Remesa', 1, '101', '2019-10-25 14:12:30', '2019-10-25 14:12:30', NULL), 
+(5, 'Cheque', 'Cheque', 1, '102', '2019-10-25 14:12:47', '2019-10-25 14:12:47', NULL), 
+(6, 'Transferencia', 'Transferencia', 1, '103', '2019-10-25 14:17:31', '2019-10-25 14:17:31', NULL) 
+
+*/

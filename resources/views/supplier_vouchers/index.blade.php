34cc05d83f463ef50df6ea3dc428ccdea50f5ebf
Markus Queridiam <mqueridiam@gmail.com>
2021-01-26 11:50:48 +0100

    Supplier Documents Autocomplete fixes

--- resources/views/supplier_vouchers/index.blade.php
+++ resources/views/supplier_vouchers/index.blade.php
@@ -361,7 +361,7 @@ $(document).ready(function() {
   $(document).ready(function() {
 
         $("#autosupplier_name").autocomplete({
-            source : "{{ route('home.searchsupplier') }}",
+            source : "{{ route('supplierinvoices.ajax.supplierLookup') }}",
             minLength : 1,
 //            appendTo : "#modalProductionOrder",
 

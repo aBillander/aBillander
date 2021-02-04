37121164248c61ac6840c504aa48a09b22d0da38
Markus Queridiam <mqueridiam@gmail.com>
2021-02-04 22:30:34 +0100

    ABCC Cataloque search misbehavior

--- app/Product.php
+++ app/Product.php
@@ -1791,7 +1791,11 @@ class Product extends Model {
 
     public function scopeIsActive($query)
     {
-        return $query->where('active', '>', 0);
+        if ( Configuration::isTrue('SHOW_PRODUCTS_ACTIVE_ONLY') )
+            // Scope already applied as Global Scope
+            return $query;
+        else
+            return $query->where('active', '>', 0);
     }
 
     public function scopeIsBlocked($query, $apply = true)

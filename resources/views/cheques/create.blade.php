3e22bd54d8180de7f8878b393925e146fb48f3ca
Markus Queridiam <mqueridiam@gmail.com>
2021-02-09 08:31:47 +0100

    Cheques create fix

--- resources/views/cheques/create.blade.php
+++ resources/views/cheques/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('title') {{ l('Parties - Create') }} @parent @stop
+@section('title') {{ l('Customer Cheques - Create') }} @parent @stop
 
 
 @section('content')
@@ -8,14 +8,14 @@
 <div class="row">
 	<div class="col-md-8 col-md-offset-2" style="margin-top: 50px">
 		<div class="panel panel-info">
-			<div class="panel-heading"><h3 class="panel-title">{{ l('New Party') }}</h3></div>
+			<div class="panel-heading"><h3 class="panel-title">{{ l('New Customer Cheque') }}</h3></div>
 			<div class="panel-body">
 
 				@include('errors.list')
 
-				{!! Form::open(array('route' => 'parties.store')) !!}
+				{!! Form::open(array('route' => 'cheques.store')) !!}
 
-					@include('parties._form')
+					@include('cheques._form')
 
 				{!! Form::close() !!}
 			</div>

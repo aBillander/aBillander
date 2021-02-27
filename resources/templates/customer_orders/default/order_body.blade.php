29fb60c61fa00cc650ab94eaa8786a77c4b4f2bf
Markus Queridiam <mqueridiam@gmail.com>
2021-02-27 13:02:40 +0100

    Typo ->mail instead of ->email

--- resources/templates/customer_orders/default/order_body.blade.php
+++ resources/templates/customer_orders/default/order_body.blade.php
@@ -83,9 +83,9 @@
             {{ $document->shippingaddress->state->name }}
 
 
-			@if ( $document->shippingaddress->mail )
+			@if ( $document->shippingaddress->email )
 
-				<div class="billing-email">{{ $document->shippingaddress->mail }}</div>
+				<div class="billing-email">{{ $document->shippingaddress->email }}</div>
 
 			@endif
             

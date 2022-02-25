04b37af209f4c2595b2e1646e50dec53344e72af
OnagroSan <onagrosan@gmail.com>
2022-02-25 10:39:05 +0100

    First migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,25 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 128)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('home_page', 128)->nullable();       // Redirect after login to route_home
+            $table->string('theme', 128)->nullable();
+            $table->string('firstname', 32)->nullable();
+            $table->string('lastname', 32)->nullable();
+//            $table->string('timezone', 32)->nullable();
+            
             $table->rememberToken();
+
+            $table->tinyInteger('is_admin')->default(0);        // Role here
+            $table->tinyInteger('active')->default(1);
+
+            $table->integer('language_id')->unsigned()->nullable(false); 
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

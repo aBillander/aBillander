987cc61cd97d10643b3db2cc7d8f7164afe197d7
Lara Billander <wasp@abillander.com>
2022-09-21 09:48:43 +0200

    Maru ninefy [modified Cpdf.pdf]

--- routes/gorrino_ninefy.php
+++ routes/gorrino_ninefy.php
@@ -17,6 +17,13 @@ use Illuminate\Support\Facades\DB;
 
 Route::get('ninefy', function()
 {
+
+// Also modified Cpdf.pdf @ line 3361 [ vendor/dompdf/dompdf/lib/Cpdf.pdf ]
+// 		file_exists( => @file_exists(
+// trata de cargar open_sans_sans_bold_xxx que no existe (por qué???)
+// el correcto es: open_sans_bold_xxx
+// Será porque PHP_ERROR_LEVEL es poco restrictivo y muestra todos los errores???
+
 	// 2022-06-27
 	$date = '2022-06-27';
 
@@ -253,6 +260,159 @@ Route::get('ninefy', function()
 
 
 /* ********************************************************** */
+
+
+Route::get('maruninefy', function()
+{
+
+
+	// 2022-05-26
+	$date = '2022-05-26';
+
+	// Watch out:
+	// https://github.com/laravel/framework/issues/22900
+
+	// Table document_ascriptions
+	// Customers
+	table_models('document_ascriptions', 'leftable_type' , ['CustomerQuotation']);
+
+//	die('OK - '.$date);
+
+	// Suppliers
+	table_models('document_ascriptions', 'leftable_type' , ['SupplierOrder', 'SupplierShippingSlip']);
+	table_models('document_ascriptions', 'rightable_type', ['SupplierShippingSlip', 'SupplierInvoice']);
+
+//	die('OK - '.$date);
+
+
+	// 2022-04-21
+	$date = '2022-04-21';
+
+	// Table stock_movements
+	$models = ['StockCountLine', 'CustomerShippingSlipLine', 'CustomerInvoiceLine', 'SupplierShippingSlipLine', 'WarehouseShippingSlipLine', 'AssemblyOrder', 'AssemblyOrderLine'];
+	foreach ($models as $model) {
+		// code...
+		Illuminate\Support\Facades\DB::statement("UPDATE `stock_movements` SET `stockmovementable_type` = 'App\\\\Models\\\\".$model."' WHERE `stockmovementable_type` = 'App\\\\".$model."';");
+
+		abi_r("UPDATE `stock_movements` SET `stockmovementable_type` = 'App\\\\Models\\\\".$model."' WHERE `stockmovementable_type` = 'App\\\\".$model."';");
+	}	
+
+//	die('OK - '.$date);
+  
+
+	// Table addresses
+	$models = ['Company', 'Customer', 'Supplier', 'Warehouse'];
+	foreach ($models as $model) {
+		// code...
+		Illuminate\Support\Facades\DB::statement("UPDATE `addresses` SET `addressable_type` = 'App\\\\Models\\\\".$model."' WHERE `addressable_type` = 'App\\\\".$model."';");
+
+		abi_r("UPDATE `addresses` SET `addressable_type` = 'App\\\\Models\\\\".$model."' WHERE `addressable_type` = 'App\\\\".$model."';");
+	}	
+
+//	die('OK - '.'addresses');
+
+	// 2022-04-09
+	$date = '2022-04-09';
+/*
+	DB::statement("drop table if exists `action_types`;");
+
+	DB::statement("create table `action_types` (`id` int unsigned not null auto_increment primary key, `alias` varchar(16) not null, `name` varchar(64) not null, `description` text null, `active` tinyint not null default '1', `position` int unsigned not null default '0', `created_at` timestamp null, `updated_at` timestamp null, `deleted_at` timestamp null) default character set utf8mb4 collate 'utf8mb4_unicode_ci';");
+*/
+	DB::statement("INSERT INTO `action_types` (`id`, `alias`, `name`, `description`, `active`, `position`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'CALL', 'Llamar por teléfono', NULL, 1, 0, NULL, NULL, NULL),
+(2, 'MEET', 'Reunión', NULL, 1, 0, NULL, NULL, NULL),
+(3, 'MEET-WEB', 'Reunión online', NULL, 1, 0, NULL, NULL, NULL);");
+/*
+	DB::statement("drop table if exists `actions`;");
+
+	DB::statement("create table `actions` (`id` int unsigned not null auto_increment primary key, `name` varchar(255) not null, `description` text null, `status` varchar(32) not null default 'pending', `priority` varchar(32) not null default 'low', `start_date` datetime null, `due_date` datetime null, `finish_date` datetime null, `results` text null, `position` int unsigned not null default '0', `user_created_by_id` int unsigned null, `user_assigned_to_id` int unsigned null, `action_type_id` int unsigned not null, `sales_rep_id` int unsigned null, `contact_id` int unsigned null, `customer_id` int unsigned null, `lead_id` int unsigned null, `created_at` timestamp null, `updated_at` timestamp null) default character set utf8mb4 collate 'utf8mb4_unicode_ci'");
+
+
+
+	DB::statement("drop table if exists `contacts`;");
+
+	DB::statement("create table `contacts` (`id` int unsigned not null auto_increment primary key, `firstname` varchar(32) not null, `lastname` varchar(32) null, `job_title` varchar(255) null, `type` varchar(32) null default 'Employee', `email` varchar(255) null, `phone` varchar(32) null, `phone_mobile` varchar(32) null, `address` varchar(255) null, `website` varchar(128) null, `is_primary` tinyint not null default '0', `blocked` tinyint not null default '0', `active` tinyint not null default '1', `notes` text null, `user_created_by_id` int unsigned not null, `party_id` int unsigned null, `customer_id` int unsigned null, `address_id` int unsigned null, `created_at` timestamp null, `updated_at` timestamp null) default character set utf8mb4 collate 'utf8mb4_unicode_ci'");
+
+
+	DB::statement("ALTER TABLE `parties` CHANGE `email` `email` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;");
+*/
+//	die('OK - '.$date);
+
+
+
+	// 2022-03-16
+	$date = '2022-03-16';
+
+	// SELECT DISTINCT column1, column2, ... FROM table_name;
+
+	// Table email_logs
+	table_models('email_logs', 'userable_type', ['CustomerUser', 'SalesRepUser', 'User']);
+
+	abi_r('OK');
+
+	// Table shipping_model_attachments
+	table_models('model_attachments', 'attachmentable_type', ['Supplier', 'DownPayment', 'Product', 'Lot', 'Customer', 'Cheque']);
+
+	// Table shipping_method_service_lines
+	table_models('shipping_method_service_lines', 'tabulable_type', ['ShippingMethod']);
+
+	// Table shipping_methods
+	table_models('shipping_methods', 'class_name', ['ShippingMethods\\\\StorePickUpShippingMethod', 'ShippingMethods\\\\RegularDeliveryRouteShippingMethod', 'ShippingMethods\\\\TransportAgencyShippingMethod']);
+
+	abi_r('OK');
+
+	// Table stock_movements
+	table_models('stock_movements', 'stockmovementable_type', ['CustomerShippingSlipLine']);
+
+	abi_r('OK');
+
+	// Table payments
+	table_models('payments', 'paymentable_type'  , ['CustomerInvoice', 'SupplierInvoice']);
+	table_models('payments', 'paymentorable_type', ['Customer', 'Supplier']);
+
+//	die('OK');
+
+	// Table images
+	table_models('images', 'imageable_type', ['Product']);
+
+	// Table document_ascriptions
+	table_models('document_ascriptions', 'leftable_type' , ['CustomerOrder', 'CustomerShippingSlip']);
+	table_models('document_ascriptions', 'rightable_type', ['CustomerOrder', 'CustomerShippingSlip', 'CustomerInvoice']);
+
+//	die('OK');
+	
+
+	// Table bank_accounts
+	$table = 'bank_accounts';
+	$field = 'bank_accountable_type';
+	$models = ['Company', 'Customer'];
+	foreach ($models as $model) {
+		// code...
+		DB::statement("UPDATE `$table` SET `$field` = 'App\\\\Models\\\\$model' WHERE `$field` = 'App\\\\$model';");
+
+		abi_r("UPDATE `$table` SET `$field` = 'App\\\\Models\\\\$model' WHERE `$field` = 'App\\\\$model';");
+	}
+
+//	die('OK - '.$date);
+
+
+	// 2022-03-15
+	$date = '2022-03-15';
+
+
+
+	// Table addresses
+	$models = ['Company', 'Customer', 'Supplier', 'Warehouse'];
+	foreach ($models as $model) {
+		// code...
+		Illuminate\Support\Facades\DB::statement("UPDATE `addresses` SET `addressable_type` = 'App\\\\Models\\\\".$model."' WHERE `addressable_type` = 'App\\\\".$model."';");
+
+		abi_r("UPDATE `addresses` SET `addressable_type` = 'App\\\\Models\\\\".$model."' WHERE `addressable_type` = 'App\\\\".$model."';");
+	}	
+
+	die('OK - '.$date);
+
+});
 
 
 
